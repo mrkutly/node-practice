3a51dfa115442c727bf21a4f16b38db10b4391cc
mark sauer-utley <mark.utley.713@gmail.com>
2018-11-24 12:07:07 -0500

    set port var

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
 const app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -45,6 +46,6 @@ app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'Unable to fulfill this request' })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server running on port 3000')
 })
